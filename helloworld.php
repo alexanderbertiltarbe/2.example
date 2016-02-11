be52240fdc9356705b793a1e6da51c152b3e3ca5
aletar <aletar@greeny.cs.tlu.ee>
2016-02-11 12:54:16 +0200

    added all from php

--- helloworld.php
+++ helloworld.php
@@ -10,5 +10,23 @@
 	// for adding up numbers + sign
 	// for adding up strings .
 	echo $first_name." ".$last_name;
+	
+	// > greater than
+	// < smaller than
+	// >= greater than or equal
+	// <= smaller than or equal
+	// == equal
+	// != not equal
+	
+	$age = 5;
+	
+	//if age is smaller than or equal to 6l, then you can sleep at lunchtime
+	if($age <= 6){
+		//it is true
+		echo " yes you can sleep at lunchtime";
+	}else{
+		//it is false
+		echo " no, you are at school";
+	}
 
 ?>
