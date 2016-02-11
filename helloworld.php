a7d63f8189ea781bf32843effe0051300c3a7bf2
aletar <aletar@greeny.cs.tlu.ee>
2016-02-11 12:43:28 +0200

    added first lines of php

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,14 @@
-hello
+<?php
+
+	// Variable starts with $,
+	//string has to use quotation marks
+	//line ends with semicolon
+	$first_name = "Bertil";
+	$last_name = "Tarbe";
+	
+	// show code to browser
+	// for adding up numbers + sign
+	// for adding up strings .
+	echo $first_name." ".$last_name;
+
+?>
